65c6acb65e7d6a890c6e202e7d571a9bfa8d2153
Iulia Gatea <iulia.gatea@clj-mb-280.3pillar.corp>
2018-10-02 17:10:03 +0300

    Edit Hamming Exercise
    Fixed indentation
    
    Fixed indentation
    
    Edited Hammimg exercise according to suggestions
    
    Changes inside compute method
    
    removed .idea
    
    Made compare_sequences method private
    
    Changed compare_sequence method

--- Ruby/hamming/hamming.rb
+++ Ruby/hamming/hamming.rb
@@ -1,15 +1,29 @@
 class Hamming
-	def self.compute(s1, s2)
-		raise ArgumentError.new("Sequencies with different length!") if s1.length != s2.length
-		a1 = s1.split("")
-		a2 = s2.split("")
-		i = 0
-		distance = 0
-		while i< s1.length do
-			distance += 1 if a1[i] != a2[i]
-			i += 1
-		end 		
-		distance
-	end
-end
 
+  def self.compute(sequence1, sequence2)
+    new(sequence1, sequence2).compute
+  end
+
+  def initialize(sequence1, sequence2)
+    @sequence1 = sequence1
+    @sequence2 = sequence2
+  end
+
+  def compute
+    raise ArgumentError, "Sequencies with different length!" unless sequence1.length == sequence2.length
+    return 0 if sequence1 == sequence2
+    compare_sequences
+  end
+
+  private
+
+  attr_reader :sequence1, :sequence2
+
+  def compare_sequences
+    sequence1_chars = @sequence1.chars
+    sequence2_chars = @sequence2.chars
+    combined = sequence1_chars.zip(sequence2_chars)
+    combined.select { |seq1_value, seq2_value| seq1_value != seq2_value }.count
+  end
+
+end
