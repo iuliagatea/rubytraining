85f085227c110247cbc81e75ec15a3d4ec7deb65
Iulia Gatea <juliagatea@yahoo.com>
2018-10-05 14:07:52 +0300

    Shrinked Raindrops class

--- Ruby/raindrops/raindrops.rb
+++ Ruby/raindrops/raindrops.rb
@@ -1,7 +1,7 @@
 class Raindrops
 
   FACTORS_OF_ITEREST = [3, 5, 7]
-  RAINDROP_SOUND = { '3' => 'Pling', '5' => 'Plang', '7' => 'Plong' }
+  RAINDROP_SOUND = { '3': 'Pling', '5': 'Plang', '7': 'Plong' }
 
   def self.convert(number)
     new(number).to_raindrop_speak
@@ -13,18 +13,12 @@ class Raindrops
 
   def to_raindrop_speak
     raise ArgumentError, "The parameter should be a number" unless number.is_a? Numeric
-    existing_factors_in_divisors = (divisors_of & FACTORS_OF_ITEREST)
-    existing_factors_in_divisors.empty? ?
-      number.to_s :
-      existing_factors_in_divisors.map { |factor| RAINDROP_SOUND[factor.to_s] }.join('')
+    sound = FACTORS_OF_ITEREST.map { |factor| RAINDROP_SOUND[:"#{factor}"] if number % factor == 0 }.join('')
+    sound.empty? ? number.to_s : sound
   end
 
   private
 
   attr_reader :number
-
-  def divisors_of
-    (1..number).select { |n| number % n == 0 }
-  end
 
 end
