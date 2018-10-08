98847837eee4336f8e4c99e09a1614f88286df35
Iulia Gatea <juliagatea@yahoo.com>
2018-10-08 12:03:02 +0300

    Updated execrise method sum_of_squares

--- Ruby/difference-of-squares/difference_of_squares.rb
+++ Ruby/difference-of-squares/difference_of_squares.rb
@@ -1,16 +1,4 @@
 class Squares
-  def self.square_of_sum(first_n_numbers)
-    new(first_n_numbers).square_of_sum
-  end
-
-  def self.sum_of_squares(first_n_numbers)
-    new(first_n_numbers).sum_of_squares
-  end
-
-  def self.difference(first_n_numbers)
-    new(first_n_numbers).difference
-  end
-
   def initialize(first_n_numbers)
     @first_n_numbers = first_n_numbers
   end
@@ -20,7 +8,7 @@ class Squares
   end
 
   def sum_of_squares
-    (1..first_n_numbers).map { |number| number**2 }.inject(0) { |sum, square| sum + square }
+    (1..first_n_numbers).inject(0) { |sum, number| sum + number**2 }
   end
 
   def difference
