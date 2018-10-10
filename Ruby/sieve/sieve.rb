1a624e00a93b06c54d668f7e38534c32f9b7af0c
Iulia Gatea <juliagatea@yahoo.com>
2018-10-10 13:51:11 +0300

    Changed primes method with Sieve of Eratosthenes algorithm

--- Ruby/sieve/sieve.rb
+++ Ruby/sieve/sieve.rb
@@ -5,15 +5,22 @@ class Sieve
   end
 
   def primes
-    (1..first_n_numbers).select { |number| count_divisors_of(number) == 2 }
+    numbers = (2..first_n_numbers).to_a
+    not_prime = []
+    primes = []
+    numbers.each_with_index do |number, index|
+      next if not_prime.include?(number)
+      primes << number
+      ((primes.last)..first_n_numbers).step(primes.last).each_with_index do |num, index|
+        next if index == 0 || num == primes.last
+        not_prime << num
+      end
+    end
+    primes
   end
 
   private
 
   attr_reader :first_n_numbers
-
-  def count_divisors_of(number)
-    (1..number).select { |factor| (number % factor).zero? }.count
-  end
 
 end
