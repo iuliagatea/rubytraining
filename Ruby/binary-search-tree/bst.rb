96b40e2d0d0e3f5858644c3dec1d81a184b44951
iuliagatea <juliagatea@yahoo.com>
2018-10-18 09:37:56 +0300

    Solved Binary Search Tree Exercise (#17)
    * Solved Binary Search Tree Exercise
    
    * Changed Bst class
    
    * Changed initialize method

---
+++ Ruby/binary-search-tree/bst.rb
@@ -0,0 +1,34 @@
+class Bst
+
+  attr_reader :data, :left, :right
+
+  def initialize(data)
+    @data = data
+  end
+
+  def insert(element)
+    if element <= data
+      insert_left(element)
+    else
+      insert_right(element)
+    end
+  end
+
+  def each(&block)
+    return enum_for(:each) unless block_given?
+    left.each(&block) if left
+    yield(data)
+    right.each(&block) if right
+  end
+
+  private
+
+  def insert_left(element)
+    left ? left.insert(element) : @left = Bst.new(element)
+  end
+
+  def insert_right(element)
+    right ? right.insert(element) : @right = Bst.new(element)
+  end
+
+end
